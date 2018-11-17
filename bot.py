cfc487a68ba53aa2fbf1322a44c600f0bdce9045
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-17 14:31:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,12 +22,38 @@ async def hosting():
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
     embed.set_author(name='Hosting')
-    embed.set_image(url = 'https://cdn.discordapp.com/attachments/444585767210057728/513149361027284992/1_Bwp5z9qlQAor4hw2K61gIw.jpeg')
-    embed.add_field(name = '``Support Server`` ',value ='https://discord.gg/A36parZ',inline = False)
-    embed.add_field(name = 'Github',value ='https://www.github.com',inline = False)
-    embed.add_field(name = 'Heroku',value ='https://heroku.com',inline = False)
     
+    embed.add_field(name = '``Support Server`` ',value ='https://discord.gg/V6EA2M',inline = False)
+    embed.add_field(name = 'Github',value ='https://www.github.com',inline = False)
+    embed.add_field(name = 'Heroku',value ='https://www.heroku.com',inline = False)
     await client.say(embed=embed)
     
+@client.command()
+async def help():
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Help')
+    
+    embed.add_field(name = '``Žádné příkazy nebily nalezeny! (Zatim)`` ',value ='Support Server: https://discord.gg/V6EA2M',inline = False)
+    
+@client.event
+async def on_member_join(member):
+    print("In our server" + member.name + " just joined")
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Welcome message')
+    embed.add_field(name = '__Vítej na MineTeam CZ SK!__',value ='**Přečti si pravidla a neporušuj je! Doufám že budeš aktivní! :)',inline = False)
+    embed.set_image(url = 'https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif')
+    await client.send_message(member,embed=embed)
+    print("Sent message to " + member.name)
+    channel = discord.utils.get(client.get_all_channels(), server__name='MineTeam CZ SK', name='welcome')
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check <#474572305192845312> and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
+    embed.add_field(name='__Díky že ses připojil!__', value='**Doufám že tu budeš aktivní ;).**', inline=True)
+    embed.add_field(name='Připojil ses jako', value=member.joined_at)
+    embed.set_image(url = 'https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif')
+    embed.set_thumbnail(url=member.avatar_url)
+    await client.send_message(channel, embed=embed)
 
+    
 client.run(os.getenv("BOT_TOKEN"))
